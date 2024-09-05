92f211ea3834de33db5234256bfac6fb9ab44f0e
Thanhntmany <thanhntmany@gmail.com>
2024-09-06 00:11:28 +0700

    ờmmmmmmm

--- filephu.c
+++ filephu.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int hamphu() {
+int hamphu(int a, int b, int c) {
 	printf("Vi du goi ham ben ngoai file hellworld\n");
 	return 0;
 };
--- hellworld.c
+++ hellworld.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-int hamphu();
+int hamphu(int a, int b, int c);
 
 int main() {
 	printf("Kha Canh khi nao man han tu vay em?\n");
-	printf("--> Nao anh vao thi em duoc ra\n")
-	hamphu();
+	printf("--> Nao anh vao thi em duoc ra\n");
+	hamphu(1, 2, 3);
+
 	return 0;
 };
