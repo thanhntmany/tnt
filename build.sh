d6860734719440b9d4de9a21d7526570ca507195
Thanhntmany <thanhntmany@gmail.com>
2024-09-05 23:32:16 +0700

    ok

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 # hello <-- helloworld.c
 mkdir -p dst
 cd dst
-gcc -o hello ../hellworld.c
-gcc -o hello-assembly ../hellworld.c -save-temps
+gcc -o hello ../hellworld.c ../filephu.c
+gcc -o hello-assembly ../hellworld.c ../filephu.c -save-temps
 
 # run hello
 ./hello
---
+++ filephu.c
@@ -0,0 +1,3 @@
+int hamphu() {
+	return 0;
+};
